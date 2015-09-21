4c9e73824f38767cd48b09b13fd9d3358b211bda
EvAlex <evalexhimself@gmail.com>
2015-09-21 13:39:52 +0300

    locales init

---
+++ clang/js/app.js
@@ -0,0 +1,15 @@
+﻿
+//var lang = 'ru';
+//if (window.location.search && window.location.search.indexOf('lang=') !== -1) {
+//    lang = window.location.search
+//        .split('&')
+//        .filter(function (e) { return e.indexOf('lang=') !== -1 })[0]
+//        .substring(6);
+//}
+i18n.init({ detectLngQS: 'lang', fallbackLng: 'en' }, function (err, t) {
+    // translate nav
+    //$(".nav").i18n();
+
+    // programatical access
+    //var appName = t("app.name");
+});
