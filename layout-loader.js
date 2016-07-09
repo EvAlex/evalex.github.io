8fde3d3ad441d6f7964fcc53fa83e52e6e91256d
EvAlex <evalexhimself@gmail.com>
2016-07-09 18:07:27 +0300

    implement layout loader

--- layout-loader.js
+++ layout-loader.js
@@ -2,25 +2,105 @@
 
 function LayoutLoader() {
     var httpBackend = new HttpBackend(),
+        actualSettings = null,
         self = this;
 
     this.loadLayout = loadLayout;
     this.defaultSettings = getDefaultSettings();
 
     function loadLayout(settings) {
-        settings = mergeWithDefaultSettings(settings);
-        httpBackend.get(settings.url, onLayoutLoaded)
+        actualSettings = mergeWithDefaultSettings(settings);
+        httpBackend.get(actualSettings.url, onLayoutLoaded)
     }
 
-    function onLayoutLoaded(err, layout) {
+    function onLayoutLoaded(err, layoutHtml) {
         if (err) {
             return console.error('Failed to load layout.', err);
         }
-        debugger;
-        var doc = new HtmlDocument(layout);
-        document.open();
-        document.write(layout);
-        document.close();
+        var layout = new HtmlDocument(layoutHtml),
+            partial = new HtmlDocument(document.documentElement);
+        mergeDocuments(partial, layout);
+    }
+
+    function mergeDocuments(partial, layout) {
+        for (var i = 0; i < actualSettings.mergeSections.length; i++) {
+            var s = actualSettings.mergeSections[i];
+            mergeDocumentsSection(
+                partial.querySelector(s.partial),
+                layout.querySelector(s.layout));
+        }
+        for (var i = 0; i < actualSettings.insertSections.length; i++) {
+            var s = actualSettings.insertSections[i],
+                layoutBody = layout.querySelector('body'),
+                partialBody = partial.querySelector('body'),
+                layoutSection = layout.querySelector(s.layout),
+                partialSection = partial.querySelector(s.partial);
+            while (layoutSection.firstChild) {
+                layoutSection.removeChild(layoutSection.firstChild);
+            }
+            while (partialSection.firstChild) {
+                layoutSection.appendChild(partialSection.firstChild);
+            }
+            while (layoutBody.firstChild) {
+                partialBody.appendChild(layoutBody.firstChild);
+            }            
+        }
+    }
+
+    /**
+     * @param {HTMLElement} elementInPartial
+     * @param {HTMLElement} elementInLayout
+     */
+    function mergeDocumentsSection(elementInPartial, elementInLayout) {
+        /*
+        for (var j = 0; j < elementInPartial.childNodes.length; j++) {
+            var np = elementInPartial.childNodes.item(j),                
+                match = null
+            for (var i = 0; i < elementInLayout.childNodes.length && match === null; i++) {
+                var nl = elementInLayout.childNodes.item(i);
+                if (elementsAreSame(np, nl)) {
+                    match = nl;
+                }
+            }
+            if (match === null) {
+                elementInLayout.appendChild(np);
+            }
+        }
+        */
+        for (var i = 0; i < elementInLayout.childNodes.length; i++) {
+            var nl = elementInLayout.childNodes.item(i),
+                match = null;
+            for (var j = 0; j < elementInPartial.childNodes.length && match === null; j++) {
+                var np = elementInPartial.childNodes.item(j);
+                if (elementsAreSame(np, nl)) {
+                    match = np;
+                }
+            }
+            if (match === null) {
+                elementInPartial.appendChild(nl);
+            }
+        }
+    }
+
+    /**
+     * @param {HTMLElement} e1
+     * @param {HTMLElement} e2
+     */
+    function elementsAreSame(e1, e2) {
+        var res = true;
+        res = res && e1.nodeName === e2.nodeName;
+        res = res && (!e1.hasAttribute && !e2.hasAttribute || !e1.hasAttribute('id') && !e2.hasAttribute('id') || e1.id === e2.id);
+        if (e1.nodeName.toLowerCase() === 'meta' && e1.nodeName === e2.nodeName) {
+            res = res && (!e1.hasAttribute && !e2.hasAttribute || !e1.hasAttribute('charset') && !e2.hasAttribute('charset') || e1.charset === e2.charset)
+            res = res && (!e1.hasAttribute && !e2.hasAttribute || !e1.hasAttribute('name') && !e2.hasAttribute('name') || e1.name === e2.name)
+        }
+        if (e1.nodeName.toLowerCase() === 'script' && e1.nodeName === e2.nodeName) {
+            res = res && (!e1.hasAttribute && !e2.hasAttribute || !e1.hasAttribute('src') && !e2.hasAttribute('src') || e1.src === e2.src)
+        }
+        if (e1.nodeName.toLowerCase() === 'link' && e1.nodeName === e2.nodeName) {
+            res = res && (!e1.hasAttribute && !e2.hasAttribute || !e1.hasAttribute('href') && !e2.hasAttribute('href') || e1.href === e2.href)
+        }
+        return res;
     }
 
     function mergeWithDefaultSettings(settings) {
@@ -33,12 +113,14 @@ function LayoutLoader() {
             url: '/',
             mergeSections: [
                 {
-                    src: 'head',
-                    dest: 'head'
-                },
+                    partial: 'head',
+                    layout: 'head'
+                }
+            ],
+            insertSections: [
                 {
-                    src: 'body',
-                    dest: '#partial-content'
+                    partial: 'body',
+                    layout: '#partial-content'
                 }
             ]
         }
@@ -68,9 +150,17 @@ function LayoutLoader() {
 }
 
 function HtmlDocument(htmlString) {
-    this.html = getElement(htmlString, 'html');
+    var html = typeof htmlString === 'string'
+        ? getElement(htmlString, 'html')
+        : htmlString;
+
+    this.querySelector = querySelector;
 
     //////////////////////////////////////////////////////////
+
+    function querySelector(selector) {
+        return html.querySelector(selector);
+    }
 
     function getElement(htmlString, name) {
         var tag = document.createElement(name);
